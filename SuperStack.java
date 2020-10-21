e91fb47ed42fd4d87356b5f9ad630b6685d7dc4b
Violet Oh <violet.oh16@gmail.com>
2020-10-21 16:35:47 -0500

    Make a stack

---
+++ SuperStack.java
@@ -0,0 +1,33 @@
+
+public class SuperStack<T> {
+	private T[] items;
+	private int top;
+	
+	public SuperStack<T> (int size) {
+		// items = new T[]; 
+		// Cannot generate generic array
+		items = (T[]) new Object[size];
+	}
+	
+	public boolean isEmpty() {
+		//TODO
+		return true;
+	}
+	
+	public boolean isFull() {
+		//TODO
+		return true;
+	}
+	
+	public boolean push(T item) {
+		//TODO
+		return true;
+	}
+	
+	public T pop() {
+		//TODO
+		return null;
+		
+	}
+	
+}
